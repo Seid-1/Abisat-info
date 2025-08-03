80b839181d3e4b91e5ef3f37f27a1031f6ee274f
seid 1 <entertainment21@gmail.com>
2025-08-03 21:47:34 +0300

    Updated database connection for Clever Cloud

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 const mysql2 = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  database: "superdish",
-  password: "@#?Seidnur12345",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 3306,
 });
 
 module.exports = pool.promise();
